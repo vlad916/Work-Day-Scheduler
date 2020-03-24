d5ea7a6ec53e1e0a4bd83e672ad997f44f779151
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-03-24 17:58:16 +1030

    Function to update time created.

--- script.js
+++ script.js
@@ -3,11 +3,32 @@ $(document).ready(function () {
 // Variables created to be manipulated
 var showDateEl = $("#currentDay");
 var BtnEl = $(".saveBtn");
-var plansEl = $(".time-block");
+var plansEl = $(".event-plan");
 
 
 showDateEl.text(moment().format("ddd, MMM Do"));
 
-plansEl.each(function)
+var currentTime = function () {
+    showDateEl.text(moment().format("ddd, MMM Do"));
+    
+    var updateTime = moment().hour();
+
+    plansEl.each(function (index, element) {
+    element = $(element);
+    if (updateTime > element.attr("data-time")) {
+        element.addClass("past").removeClass("future");
+    } else if (updateTime === element.attr("data-time")) {
+        element.addClass("present").removeClass("future");
+    }  
+});
+}   
+
+
+
+
+
+
+
+
 
 });
